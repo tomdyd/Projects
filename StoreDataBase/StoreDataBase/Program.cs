99cdace8a33a77615260babe18bd1b7c7915bc0b
Tomasz Dyda <tomasz.dyda.chem@gmail.com>
2023-05-30 19:54:53 +0200

    update

--- StoreDataBase/StoreDataBase/Program.cs
+++ StoreDataBase/StoreDataBase/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using MongoDB.Driver;
+using StoreDataBase;
+
+string connectionString = "mongodb://127.0.0.1:27017";
+string dataBaseName = "simple_db";
+string collectionName = "people";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(dataBaseName);
+var collection = db.GetCollection<PersonModel>(collectionName);
+
+var person = new PersonModel { FirstName = "Tomasz", LastName = "Dyda" };
+
+await collection.InsertOneAsync(person);
+
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.Id} {result.FirstName} {result.LastName}");
+}
