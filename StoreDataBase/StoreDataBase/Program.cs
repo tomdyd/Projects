e3d8b8395b882c5488e224b58360955b5f204a9e
Tomasz Dyda <tomasz.dyda.chem@gmail.com>
2023-05-31 19:47:18 +0200

    adding CRUD

--- StoreDataBase/StoreDataBase/Program.cs
+++ StoreDataBase/StoreDataBase/Program.cs
@@ -1,21 +1,30 @@
-﻿using MongoDB.Driver;
-using StoreDataBase;
+﻿//using MongoDB.Driver;
+//using StoreDataBase;
 
-string connectionString = "mongodb://127.0.0.1:27017";
-string dataBaseName = "simple_db";
-string collectionName = "people";
+//string connectionString = "mongodb://127.0.0.1:27017";
+//string dataBaseName = "simple_db";
+//string collectionName = "people";
 
-var client = new MongoClient(connectionString);
-var db = client.GetDatabase(dataBaseName);
-var collection = db.GetCollection<PersonModel>(collectionName);
+//var client = new MongoClient(connectionString);
+//var db = client.GetDatabase(dataBaseName);
+//var collection = db.GetCollection<PersonModel>(collectionName);
 
-var person = new PersonModel { FirstName = "Tomasz", LastName = "Dyda" };
+//var person = new PersonModel { FirstName = "Tomasz", LastName = "Dyda" };
 
-await collection.InsertOneAsync(person);
+//await collection.InsertOneAsync(person);
 
-var results = await collection.FindAsync(_ => true);
+//var results = await collection.FindAsync(_ => true);
 
-foreach (var result in results.ToList())
-{
-    Console.WriteLine($"{result.Id} {result.FirstName} {result.LastName}");
-}
+//foreach (var result in results.ToList())
+//{
+//    Console.WriteLine($"{result.Id} {result.FirstName} {result.LastName}");
+//}
+
+
+
+using MongoDataAccess.DataAccess;
+using MongoDataAccess.Models;
+
+ChoreDataAccess db = new ChoreDataAccess();
+
+await db.CreateUser(new UserModel { FirstName = "Tomasz", LastName = "Dyda" });
