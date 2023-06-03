3441f19c8339817aa29409e3598f2f5b27667940
Tomasz Dyda <tomasz.dyda.chem@gmail.com>
2023-06-03 22:10:18 +0200

    backup previous version

--- DataBase/StoreDataAccess/DataAccess/DataAccess.cs
+++ DataBase/StoreDataAccess/DataAccess/DataAccess.cs
@@ -5,11 +5,7 @@ using StoreDataAccess.Models;
 namespace StoreDataAccess.DataAccess;
 public class DataAccess
 {
-<<<<<<< HEAD
     private const string _connectionString = "mongodb://localhost:27017";
-=======
-    private const string _connectionString = "mongodb://localhost:27017/";
->>>>>>> 273c1da7af2c7fc547a30faefdd6187e7f855bc6
     private const string _databaseName = "Store_db";
     private const string _valveCollection = "valve_collection";
     private const string _canCollection = "can_collection";
@@ -64,7 +60,6 @@ public class DataAccess
     public Task UpdateValve(ValveModel valve)
     {
         var valveCollection = ConnectToMongo<ValveModel>(_valveCollection);
-<<<<<<< HEAD
         var filter = Builders<ValveModel>.Filter.Eq("_index", valve._index);
         return valveCollection.ReplaceOneAsync(filter, valve, new ReplaceOptions { IsUpsert = true });
     }
@@ -74,10 +69,6 @@ public class DataAccess
         var canCollection = ConnectToMongo<CanModel>(_canCollection);
         var filter = Builders<CanModel>.Filter.Eq("_index", can._index);
         return canCollection.ReplaceOneAsync(filter, can, new ReplaceOptions { IsUpsert = true});
-=======
-        var filter = Builders<ValveModel>.Filter.Eq("_index", valve.GetIndex());
-        return valveCollection.ReplaceOneAsync(filter, valve, new ReplaceOptions { IsUpsert = true});
->>>>>>> 273c1da7af2c7fc547a30faefdd6187e7f855bc6
     }
 
 }
